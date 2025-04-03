3637e2f3c73d6197648f5cfe488d20b87e97b930
Joffrey Bocquet <jbocquet@me.com>
2025-04-03 10:08:41 +0200

    Initial Commit

---
+++ ContactsTechAppTests/ContactsTechAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsTechAppTests.swift
+//  ContactsTechAppTests
+//
+//  Created by Joffrey Bocquet on 03/04/2025.
+//
+
+import Testing
+@testable import ContactsTechApp
+
+struct ContactsTechAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
